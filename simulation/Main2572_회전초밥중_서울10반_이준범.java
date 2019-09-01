9a8e642855c685d1cc0ff05f2a814e5c52afd101
Junboom <ticktock0414@naver.com>
2019-09-01 22:28:32 +0900

    20190901_first_JB

---
+++ simulation/Main2572_회전초밥중_서울10반_이준범.java
@@ -0,0 +1,48 @@
+package com.ssafy.algo;
+
+import java.io.*;
+import java.util.*;
+
+public class Main2572_회전초밥중_서울10반_이준범 {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int ans = 0;
+		int n = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int[] kind = new int[d+1];
+		int[] sushi = new int[n+k-1];
+		for (int i = 0; i < n; i++)
+			sushi[i] = Integer.parseInt(br.readLine());
+		for (int i = 0; i < k-1; i++)
+			sushi[n+i] = sushi[i];
+		
+		int cnt = 0;
+		for (int i = 0; i < k; i++) {
+			if (kind[sushi[i]] == 0)		cnt++;
+			kind[sushi[i]]++;
+		}
+		if (kind[c] == 0)	cnt++;
+		kind[c]++;
+		ans = cnt;
+		
+		for (int i = 1; i < n; i++) {
+			if (ans == k+1)	break;
+			kind[sushi[i-1]]--;
+			if (kind[sushi[i-1]] == 0)		cnt--;
+			if (kind[sushi[i+k-1]] == 0)	cnt++;
+			kind[sushi[i+k-1]]++;
+			ans = (ans < cnt) ? cnt : ans;
+		}
+		
+		bw.write(String.valueOf(ans));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	
+}
